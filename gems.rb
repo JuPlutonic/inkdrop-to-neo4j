76b8b1b2e75e4838ccd1013c1a2563c3b72ff911
azhevlakov <azhevlakov@ceki.ru>
2021-02-10 17:47:28 +0300

    First⭐[1] commit

---
+++ gems.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'pp'
+
+gem 'rubocop', require: false
+gem 'overcommit', require: false
---
+++ main.rb
@@ -0,0 +1,6 @@
+require 'pp'
+require 'json'
+
+# require_relative './neo4j'
+
+pp Dir.glob('./data/*.json')

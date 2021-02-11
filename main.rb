f3ca0545e86318c5308d267a6cfce6a30d168524
azhevlakov <azhevlakov@ceki.ru>
2021-02-11 04:06:44 +0300

    Added⭐[3] pattern matching

--- main.rb
+++ main.rb
@@ -6,3 +6,26 @@ require 'json'
 files = Dir.glob('./data/*.json').map do |file_name|
   JSON.parse(File.read(file_name))
 end
+
+inkdrop = {
+  notes: [],
+  tags: [],
+  books: []
+}
+
+files.each do |file|
+  case file['_id'].split(':')
+  in ['note', _id]
+    inkdrop[:notes] << file
+  in ['tag', _id]
+    inkdrop[:tags] << file
+  in ['book', _id]
+    inkdrop[:books] << file
+  else
+    # nothing
+  end
+end
+
+# puts for debugging
+puts "notes: #{inkdrop[:notes].count} tags: #{inkdrop[:tags].count} books: #{inkdrop[:books].count}"
+

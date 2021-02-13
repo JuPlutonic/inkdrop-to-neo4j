4eceb8f7a43646fbb1b191551d52370854cb5f99
azhevlakov <azhevlakov@ceki.ru>
2021-02-13 19:11:10 +0300

    Feature⭐ cre8 note relations: parse*.rb now getting nested notes

--- parse_inkdrop_backup_folder.rb
+++ parse_inkdrop_backup_folder.rb
@@ -28,8 +28,32 @@ class ParseInkdropBackupFolder
     inkdrop[:tags].map! { |tag| { id: tag['_id'], title: tag['name'] } }
     inkdrop[:books].map! { |book| { id: book['_id'], parent_id: book['parentBookId'], title: book['name'] } }
     inkdrop[:notes].map! do |note|
-      { id: note['_id'], title: note['title'], tags: note['tags'], book_id: note['bookId'], body: note['body'] }
+      {
+        id: note['_id'],
+        title: note['title'],
+        tags: note['tags'],
+        book_id: note['bookId'],
+        body: note['body'],
+        nested_notes: note['body'].match(/\(inkdrop:\/\/note[[:punct:]](.+)\)/).captures
+      }
     end
+
+    # require 'debug'
+    # Push n <enter> n <enter> n <enter> and debugging cursor will be placed
+    # on `inkdrop` line beyond the comments.
+    # Then push l and <enter> button and write
+    # noteN = inkdrop[:notes][N] − where Ns is
+    # a number (it must be a note where you see `inkdrop://note` link/links).
+    #
+    # To debug nested_notes urls, write − noteN[:body].match(/\(inkdrop:\/\/note[[:punct:]](.+)\)/)
+    #
+    # Method #to_hash_ast is to tough to use it to get aimed nested hash
+    #
+    # If you always use a related_notes template
+    # you must change the inkdrop[:notes] here above the comments
+    # Use note['body'].split('TEMPLATE TEXT FOR RELATED NOTES').first −
+    # for first of nested_notes links and note['body']
+    # .split('TEMPLATE TEXT FOR RELATED NOTES').last − for related_notes link from a template.
 
     inkdrop
   end

6b179785d38c22eb3a532faafc920b112bb2f92b
azhevlakov <azhevlakov@ceki.ru>
2021-02-14 01:30:48 +0300

    Feature⭐ cre8 note relations: parse*.rb now getting nested notes - refactored

--- parse_inkdrop_backup_folder.rb
+++ parse_inkdrop_backup_folder.rb
@@ -30,31 +30,35 @@ class ParseInkdropBackupFolder
     inkdrop[:notes].map! do |note|
       {
         id: note['_id'],
-        title: note['title'],
+         title: note['title'],
         tags: note['tags'],
         book_id: note['bookId'],
         body: note['body'],
-        nested_notes: note['body'].match(/\(inkdrop:\/\/note[[:punct:]](.+)\)/).captures
+        nested_notes: get_note_links(note['body'], 0)
+        #, related_notes: get_note_links(note['body'],1)
       }
     end
 
-    # require 'debug'
+    # require 'debug' # debug notes' bodies
     # Push n <enter> n <enter> n <enter> and debugging cursor will be placed
     # on `inkdrop` line beyond the comments.
     # Then push l and <enter> button and write
     # noteN = inkdrop[:notes][N] − where Ns is
     # a number (it must be a note where you see `inkdrop://note` link/links).
-    #
     # To debug nested_notes urls, write − noteN[:body].match(/\(inkdrop:\/\/note[[:punct:]](.+)\)/)
-    #
-    # Method #to_hash_ast is to tough to use it to get aimed nested hash
     #
     # If you always use a related_notes template
     # you must change the inkdrop[:notes] here above the comments
-    # Use note['body'].split('TEMPLATE TEXT FOR RELATED NOTES').first −
+    # Use note['body'].split('TEMPLATE TEXT FOR RELATED NOTES')[0] −
     # for first of nested_notes links and note['body']
-    # .split('TEMPLATE TEXT FOR RELATED NOTES').last − for related_notes link from a template.
+    # .split('TEMPLATE TEXT FOR RELATED NOTES')[1] − for related_notes link from a template.
 
     inkdrop
   end
+  private
+
+    def get_note_links(body, part)
+      match = body.split('RELATED NOTES:')[part]&.match(/\(inkdrop:\/\/note[[:punct:]](.+)\)/)
+      match ? match.captures.map { |matched_link| "note:#{matched_link}" } : []
+    end
 end
